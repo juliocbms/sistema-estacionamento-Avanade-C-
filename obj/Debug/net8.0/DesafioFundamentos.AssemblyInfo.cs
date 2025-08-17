d94a99a5ba3967f70a49dd1d652ccf3ed8602df9
Julio <juliocesar13b@gmail.com>
2025-08-17 13:34:10 -0300

    projeto final

--- obj/Debug/net8.0/DesafioFundamentos.AssemblyInfo.cs
+++ obj/Debug/net8.0/DesafioFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesafioFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af631842402b7210d11d91582c4b9744fd18b499")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb0a160cdd57a6c1f3bf36497782ee4b4a34443")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesafioFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesafioFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
